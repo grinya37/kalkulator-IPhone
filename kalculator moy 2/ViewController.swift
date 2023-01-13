2f9ac659d9e86dec7dc41876edf72a29cca5184d
grinya37 <112849355+grinya37@users.noreply.github.com>
2023-01-13 09:35:26 +0300

    Initial Commit

---
+++ kalculator moy 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kalculator moy 2
+//
+//  Created by Николай Гринько on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
